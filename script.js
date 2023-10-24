e5f4e95228bbfaec6a4a5fdeb02bc5a89c730d2c
Hedmon0094 <123468785+Hedmon0094@users.noreply.github.com>
2023-10-24 20:43:00 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDrLQ5VCiuPHLgExHTWyJydpm79cx6PxdY",
+    authDomain: "compassion-to-do-app.firebaseapp.com",
+    databaseURL: "https://compassion-to-do-app-default-rtdb.firebaseio.com",
+    projectId: "compassion-to-do-app",
+    storageBucket: "compassion-to-do-app.appspot.com",
+    messagingSenderId: "443916263362",
+    appId: "1:443916263362:web:67632bef4916cbc64bc05b",
+    measurementId: "G-JEVCX59KCG"
+  };
